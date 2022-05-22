5808b59f5c9a31771e533482e1693edc62dfb4f4
FALVO Anthony <anthony.falvo.pro@gmail.com>
2022-05-22 14:34:50 +0200

    :FEAT: => Partie chat en cour, destructuration sur la partie de recuperation URL

--- src/components/Messagerie/Messagerie.tsx
+++ src/components/Messagerie/Messagerie.tsx
@@ -1,9 +1,18 @@
-import React, 
-    {useState, useEffect } from 'react';
-import queryString         from 'query-string';
-
+import React, {useState, useEffect }  from 'react';
+import { useParams }                  from 'react-router';
+import queryString                    from 'query-string';
+import io                             from 'socket.io-client';
 
 const Messagerie = () => {
+
+    useEffect(() => {
+        const { name, room } = queryString.parse(document.location.search);
+        console.log(name, room);
+ 
+
+        
+    }, [])
+
     return (
         <div>
             Messagerie
