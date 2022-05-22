d117d32246e93daca865228145b656b96c75336e
FALVO Anthony <anthony.falvo.pro@gmail.com>
2022-05-22 15:12:45 +0200

    :FEAT: => En cours de réalisation sur la partie messagerie

--- src/components/Messagerie/Messagerie.tsx
+++ src/components/Messagerie/Messagerie.tsx
@@ -2,15 +2,22 @@ import React, {useState, useEffect }  from 'react';
 import { useParams }                  from 'react-router';
 import queryString                    from 'query-string';
 import io                             from 'socket.io-client';
+let socket :any;
 
 const Messagerie = () => {
+
+    const [name, setName] :any = useState('');
+    const [room, setRoom] :any = useState('');
+    const ENDPOINT             = 'localhost:5000';
 
     useEffect(() => {
 
         const { name, room } = queryString.parse(document.location.search);
-        
-        console.log(name, room);
- 
+
+        socket = io(ENDPOINT);
+
+        setName(name);
+        setRoom(room);
 
         
     }, [])
