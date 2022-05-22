ac9d3b3b394d13007a30ac91e91ab395f505224b
FALVO Anthony <anthony.falvo.pro@gmail.com>
2022-05-22 11:25:44 +0200

    :STYLE: => style et logique de la page se connecter

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -18,6 +18,33 @@ const SignIn = () => {
                 <h2 className='title-secondary'> 
                     Se connecter <Underscore />
                 </h2>
+                <form>
+                    <div className='sign-in-flex'>
+                        <label htmlFor="sign-in-name">Nom :</label>
+                        <input type="text" 
+                               placeholder={name} 
+                               className='sign-in-input' 
+                               id='sign-in-name'
+                               onChange={(event? :any | undefined) => {
+                                    setName(event.target.value);
+                               }} 
+                        />
+                    </div>
+                    <div className='sign-in-flex'>
+                        <label htmlFor="sign-in-room">Mot de passe :</label>
+                        <input type="text"
+                               placeholder={room} 
+                               className='sign-in-input'
+                               id='sign-in-room' 
+                               onChange={(event? :any | undefined) => {
+                                    setRoom(event.target.value);
+                                }} 
+                        />
+                    </div>
+                    <NavLink to='/messagerie' >
+                        <button type='submit' className='btn-submit-form'>S'enregistrer</button>
+                    </NavLink>
+                </form>
             </div>
         </div>
     );
