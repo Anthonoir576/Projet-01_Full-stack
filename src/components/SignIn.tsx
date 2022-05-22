e8ed7116ddc2f15f76f2bbec4670092d9d722b5a
FALVO Anthony <anthony.falvo.pro@gmail.com>
2022-05-22 10:36:32 +0200

    :STYLE: => style et logique de la page se connecter

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, 
+    {useState}     from 'react';
+import { NavLink } from 'react-router-dom';
+import Underscore  from '../icons/Underscore';
+
+
+
+
 
 const SignIn = () => {
+
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+
     return (
-        <div>
-            sign in
+        <div className='sign-in-container'>
+            <div className="sign-in">
+                <h2 className='title-secondary'> 
+                    Se connecter <Underscore />
+                </h2>
+            </div>
         </div>
     );
 };
+
+
 
 export default SignIn;
---
+++ src/icons/Underscore.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Underscore = () => {
+  return (
+    <>
+        <span className="console-underscore">
+            &#95;
+        </span>
+    </>
+  );
+};
+
+export default Underscore;
