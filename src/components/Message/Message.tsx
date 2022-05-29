9753f814af9d065f17be1ba374b8382b316298af
FALVO Anthony <anthony.falvo.pro@gmail.com>
2022-05-29 09:33:42 +0200

    :FEAT: => tj bugger - message partie

--- src/components/Message/Message.tsx
+++ src/components/Message/Message.tsx
@@ -5,19 +5,30 @@ import { useLocation } from "react-router-dom";
 
 const Message: React.FC<{ name: any; message: any }> = ( {name, message} ) => {
 
-    let userActiveSend    = false;
-    const trimName        = name.trim().toLowerCase();
+    let userActiveSend :boolean = false;
+    const trimName              = name.trim().toLowerCase();
 
     // name in url de lutilisateur en cour
-    const sampleLocation  = useLocation();
-    const user            = sampleLocation.search.split('&');
-    const newUser         = user[0].split('=');
-    const finalUser       = newUser[1];
+    const sampleLocation        = useLocation();
+    const user                  = sampleLocation.search.split('&');
+    const user2                 = user[0].split('=');
+    const finalUser             = user2[1].trim().toLowerCase();
 
-  
+
+    console.log('----URL NAME ----');
+    console.log(finalUser);
+    console.log('--- NAME DINAMIQUE-------');
+    console.log(trimName);
+    console.log('----------');
+    
+
     
     if (finalUser === trimName) {
-
+      userActiveSend = true;
+      console.log(userActiveSend);
+      
+    } else {
+      userActiveSend = false;
     };
   
 
